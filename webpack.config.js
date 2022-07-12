e3c3cda19bbbc27de0e939e2ff9e91b85de62937
Gina Henderson <gina@ginahenderson.me>
2022-07-11 23:35:56 -0500

    Set up project

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'GinaOS',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+
+            {
+
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+
+            },
+        ],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+};
