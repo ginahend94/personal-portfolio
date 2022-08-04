0174733f5288c95ed4e4450a98cd7206d5c79588
Gina Henderson <gina@ginahenderson.me>
2022-08-04 17:29:43 -0500

    Set up browser

--- src/components/app-window.js
+++ src/components/app-window.js
@@ -43,88 +43,7 @@ const File = (name, thumbnail, date, size) => {
     return container;
 }
 
-const Pages = (() => {
-    const content = (name, img, desc, title, subtitle) => {
-        const container = document.createElement('div');
-        container.classList.add('content', name);
 
-        const header = document.createElement('header');
-        container.append(header);
-        const h2 = document.createElement('h2');
-        header.append(h2);
-        h2.textContent = title;
-        const h3 = document.createElement('h3');
-        header.append(h3);
-        h3.textContent = subtitle;
-
-        const image = new Image();
-        image.src = img;
-        image.classList.add(name);
-
-        container.append(image);
-
-        const description = document.createElement('p');
-        description.innerHTML = desc;
-
-        container.append(description);
-
-        return container
-    }
-
-    const app = (link) => {
-        const obj = document.createElement('object');
-        obj.setAttribute('data', link);
-        obj.setAttribute('type', 'text/html');
-        obj.classList.add('app-screen');
-        return obj;
-    }
-
-    const diddit = content(
-        'diddit',
-        Diddit,
-        `<a href="https://ginahenderson.me/to-do-list" target="_blank">Diddit</a> is a CRUD to-do list app that utilizes local storage to maintain the user's unput. It also features UI customization features.`,
-        'Diddit',
-        'To-Do List Web App'
-    );
-
-    const raineyIceCream = content(
-        'rainey',
-        Rainey,
-        `<a href="https://ginahenderson.me/rainey-ice-cream" target="_blank">Rainey Ice Cream</a> is a single-page mock website for the fictional restaurant Rainey Ice Cream. The site is built with HTML, CSS, and Vanilla Javascript utilizing Webpack. The site also has a custom Locations tab that uses the Mapbox API and GeoJSON to create fake worldwide locations for the restaurant.`,
-        'Rainey Ice Cream',
-        'Restaurant Website'
-    )
-
-    const minecraft = content(
-        'minecraft',
-        Minecraft,
-        '<a href="https://ginahenderson.me/minecraft-guide" target = "_blank">Gina\'s Unofficial Minecraft Beginner\'s Guide</a> is a responsive website to teach novice Minecraft players how to beat the game. Vanilla CSS was used to style the page.',
-        'Gina\'s Unofficial Minecraft Guide',
-        'Instructional Website'
-    )
-    const caffeineClub = content(
-        'caffeine',
-        Caffeine,
-        `<a href="https://ginahenderson.me/sign-up-form" target="_blank">Caffeine Club</a> is a mock sign-up form. The site uses Vanilla Javascript for client-side form validation.`,
-        'Caffeine Club',
-        'Sign-up Form'
-    )
-    const ginaTharin = content(
-        'gina-tharin',
-        GinaTharin,
-        `<a href="https://ginatharin.com/" target="_blank">GinaTharin.com</a> is the official website for my side-project as a Singer-Songwriter. The site is built with HTML, CSS, and Vanilla Javascript. The site also incorporates the Twitter API to display tweets in real-time.`,
-        'Gina Tharin',
-        'Official Artist Website',
-    )
-
-    return {
-        diddit,
-        raineyIceCream,
-        minecraft,
-        caffeineClub,
-        ginaTharin,
-    };
-})()
 
 
 const appWindow = (() => {
@@ -258,6 +177,116 @@ const appWindow = (() => {
     })();
 
     const browser = (() => {
+        const header = (() => {
+            const container = document.createElement('header');
+            container.classList.add('browser-header');
+            const lock = icon('fa6-solid:lock', ['lock']);
+            const searchBar = document.createElement('div');
+            searchBar.classList.add('search-bar');
+            searchBar.setAttribute('contenteditable', true);
+            searchBar.setAttribute('spellcheck', false);
+            searchBar.textContent = 'My Work';
+            searchBar.addEventListener('blur', () => searchBar.textContent = 'My Work');
+            const refresh = icon('ic:round-refresh', ['refresh']);
+            refresh.classList.add('refresh');
+            container.append(lock, searchBar, refresh);
+            return container;
+        })();
+
+        const footer = (() => {
+            const container = document.createElement('footer');
+            container.classList.add('browser-footer');
+            const backButton = icon('eva:arrow-back-fill', ['browser-back']);
+            const forwardButton = icon('eva:arrow-forward-fill', ['browser-forward']);
+            const home = icon('fa6-solid:house-chimney', ['browser-home']);
+            home.addEventListener('click', () => console.log('will do'));
+            const tabs = icon('fluent:tabs-24-filled', ['browser-tabs']);
+            container.append(backButton, forwardButton, home, tabs);
+            return container;
+        })();
+        const Pages = (() => {
+            const content = (name, img, desc, title, subtitle) => {
+                const container = document.createElement('div');
+                container.classList.add('content', name);
+
+                const header = document.createElement('header');
+                container.append(header);
+                const h2 = document.createElement('h2');
+                header.append(h2);
+                h2.textContent = title;
+                const h3 = document.createElement('h3');
+                header.append(h3);
+                h3.textContent = subtitle;
+
+                const image = new Image();
+                image.src = img;
+                image.classList.add(name);
+
+                container.append(image);
+
+                const description = document.createElement('p');
+                description.innerHTML = desc;
+
+                container.append(description);
+
+                return container
+            }
+
+            const app = (link) => {
+                const obj = document.createElement('object');
+                obj.setAttribute('data', link);
+                obj.setAttribute('type', 'text/html');
+                obj.classList.add('app-screen');
+                return obj;
+            }
+
+            const diddit = content(
+                'diddit',
+                Diddit,
+                `<a href="https://ginahenderson.me/to-do-list" target="_blank">Diddit</a> is a CRUD to-do list app that utilizes local storage to maintain the user's unput. It also features UI customization features.`,
+                'Diddit',
+                'To-Do List Web App'
+            );
+
+            const raineyIceCream = content(
+                'rainey',
+                Rainey,
+                `<a href="https://ginahenderson.me/rainey-ice-cream" target="_blank">Rainey Ice Cream</a> is a single-page mock website for the fictional restaurant Rainey Ice Cream. The site is built with HTML, CSS, and Vanilla Javascript utilizing Webpack. The site also has a custom Locations tab that uses the Mapbox API and GeoJSON to create fake worldwide locations for the restaurant.`,
+                'Rainey Ice Cream',
+                'Restaurant Website'
+            )
+
+            const minecraft = content(
+                'minecraft',
+                Minecraft,
+                '<a href="https://ginahenderson.me/minecraft-guide" target = "_blank">Gina\'s Unofficial Minecraft Beginner\'s Guide</a> is a responsive website to teach novice Minecraft players how to beat the game. Vanilla CSS was used to style the page.',
+                'Gina\'s Unofficial Minecraft Guide',
+                'Instructional Website'
+            )
+            const caffeineClub = content(
+                'caffeine',
+                Caffeine,
+                `<a href="https://ginahenderson.me/sign-up-form" target="_blank">Caffeine Club</a> is a mock sign-up form. The site uses Vanilla Javascript for client-side form validation.`,
+                'Caffeine Club',
+                'Sign-up Form'
+            )
+            const ginaTharin = content(
+                'gina-tharin',
+                GinaTharin,
+                `<a href="https://ginatharin.com/" target="_blank">GinaTharin.com</a> is the official website for my side-project as a Singer-Songwriter. The site is built with HTML, CSS, and Vanilla Javascript. The site also incorporates the Twitter API to display tweets in real-time.`,
+                'Gina Tharin',
+                'Official Artist Website',
+            )
+
+            return {
+                diddit,
+                raineyIceCream,
+                minecraft,
+                caffeineClub,
+                ginaTharin,
+            };
+        })()
+
         
     })();
 
